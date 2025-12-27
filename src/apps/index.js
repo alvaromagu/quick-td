53cf461065f0dc7ac3d8ce4d28501a9496673be5
alvaro <alvaromagu2002@gmail.com>
2025-12-27 22:17:20 +0100

    fix: remove error exports from apps index

--- src/apps/index.js
+++ src/apps/index.js
@@ -1,6 +1,3 @@
 export { main } from './main.js';
 export { list } from './list.js';
-export { manage } from './manage.js';
-export { remove } from './remove.js';
 export { search } from './search.js';
-export { upsert } from './upsert.js';
